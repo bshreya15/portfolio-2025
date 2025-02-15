7d5ad18f74ece9641a5dbf19dd377c7668bff6a2
shreya <i.bakshi.shreya@gmail.com>
2025-02-15 21:59:36 +0000

    img quality up

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -146,6 +146,7 @@ export const AboutSection = () => {
               <Image
                 src={mapImage}
                 alt="map"
+                quality={95}
                 className="h-full w-full object-cover"
               />
               <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-24 from-emerald-300 to-sky-400 forced-color-adjust-none rounded-full bg-gradient-to-r after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
